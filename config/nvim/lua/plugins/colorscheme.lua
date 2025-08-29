c39e9e14ba044c11e4c1f441ab6636e3f21969a4
phonglh <phonghngle@gmail.com>
2025-08-30 02:31:20 +0700

    update new config for go-lang

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sainnhe/edge",
+  },
+  {
+    "sainnhe/gruvbox-material",
+  },
+  {
+    "sainnhe/everforest",
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}

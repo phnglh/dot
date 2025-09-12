1dc28672650459c3222e1ef0ccd5fb42f6d8c4a2
phonglh <phonghngle@gmail.com>
2025-09-13 06:56:07 +0700

    update

--- config/nvim/lua/plugins/rust.lua
+++ config/nvim/lua/plugins/rust.lua
@@ -1,40 +1,41 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^6", -- Recommended
-    lazy = false, -- This plugin is already lazy
-    ft = "rust",
-    config = function()
-      local mason_registry = require("mason-registry")
-      local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-      -- If you are on Linux, replace the line above with the line below:
-      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-      local cfg = require("rustaceanvim.config")
-
-      vim.g.rustaceanvim = {
-        server = {
-          cmd = function()
-            local mason_registry = require("mason-registry")
-            if mason_registry.is_installed("rust-analyzer") then
-              -- This may need to be tweaked depending on the operating system.
-              local ra = mason_registry.get_package("rust-analyzer")
-              local ra_filename = ra:get_receipt():get().links.bin["rust-analyzer"]
-              return { ("%s/%s"):format(ra:get_install_path(), ra_filename or "rust-analyzer") }
-            else
-              -- global installation
-              return { "rust-analyzer" }
-            end
-          end,
-        },
-        default_settings = {
-          ["rust-analyzer"] = {},
-        },
-      }
-    end,
-  },
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   version = "^6", -- Recommended
+  --   lazy = false, -- This plugin is already lazy
+  --   ft = "rust",
+  --   config = function()
+  --     local mason_registry = require("mason-registry")
+  --     local codelldb = mason_registry.get_package("codelldb")
+  --     local extension_path = codelldb:get_install_path() .. "/extension/"
+  --     local codelldb_path = extension_path .. "adapter/codelldb"
+  --     local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+  --     -- If you are on Linux, replace the line above with the line below:
+  --     -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+  --     local cfg = require("rustaceanvim.config")
+  --
+  --     vim.g.rustaceanvim = {
+  --       server = {
+  --         cmd = function()
+  --           local mason_registry = require("mason-registry")
+  --           if mason_registry.is_installed("rust-analyzer") then
+  --             -- This may need to be tweaked depending on the operating system.
+  --             local ra = mason_registry.get_package("rust-analyzer")
+  --             local ra_filename = ra:get_receipt():get().links.bin["rust-analyzer"]
+  --             return { ("%s/%s"):format(ra:get_install_path(), ra_filename or "rust-analyzer") }
+  --           else
+  --             -- global installation
+  --             return { "rust-analyzer" }
+  --           end
+  --         end,
+  --       },
+  --       default_settings = {
+  --         ["rust-analyzer"] = {},
+  --       },
+  --     }
+  --   end,
+  -- },
+  --
   {
     "rust-lang/rust.vim",
     ft = "rust",
